08caf3ca4b2d668741386cd9ed087024bc4b3d0a
Gaurav Singh <gauravsingh.info@gmail.com>
2020-12-18 23:31:19 +0530

    Leetcode 162 Problem Added

---
+++ Leet_Code_problems/162.find_peak_element.py
@@ -0,0 +1,48 @@
+"""
+A peak element is an element that is strictly greater than its neighbors.
+
+Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+nums[i] != nums[i + 1] for all valid i.
+ 
+
+Follow up: Could you implement a solution with logarithmic complexity?
+"""
+def findPeakElement(nums):
+    
+    n = len(nums)
+    if n == 1:
+        return 0
+    left = 0 
+    right = n-1
+    while left < right: 
+        mid = (left+right) >> 1
+        if nums[mid] < nums[mid+1]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+
+if __name__ == "__main__":
+    nums = [1,2,3,1]
+    print(findPeakElement(nums))
