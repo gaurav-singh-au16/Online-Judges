6aaf4b7502086a046698007522e13aa9c06a7317
Gaurav Singh <gauravsingh.info@gmail.com>
2021-01-11 18:52:10 +0530

    .\tv_shows.py

--- Binary_Search_Problems/tv_shows.py
+++ Binary_Search_Problems/tv_shows.py
@@ -1,3 +1,25 @@
+"""
+TV Shows
+Given a list of strings shows, a list of integers durations, and an integer k, where shows[i] and durations[i] represent the name and duration watched by the ith person, return the total duration watched of the k most watched shows.
+
+Example 1
+Input
+shows = ["Top Gun", "Aviator", "Top Gun", "Roma", "Logan"]
+durations = [5, 3, 5, 13, 4]
+k = 2
+Output
+23
+Explanation
+The top 2 most watched movies are "Roma" and "Top Gun" for total durations of 13 and 10 = 5+ 5.
+
+Solved
+486
+Attempted
+584
+Rate
+83.22%
+"""
+
 def solve(shows, durations, k):
     p1 = 0
     p2 = 0
