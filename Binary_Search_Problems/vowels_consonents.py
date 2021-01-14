41c510ceb1c1766707ff57db963ac3274235ce57
Gaurav Singh <gauravsingh.info@gmail.com>
2021-01-14 18:44:43 +0530

    .\vowels_consonents.py

--- Binary_Search_Problems/vowels_consonents.py
+++ Binary_Search_Problems/vowels_consonents.py
@@ -1,4 +1,27 @@
 """
+Vowels and Consonants Sort
+Given a lowercase alphabet string s, return a string with all the vowels of s in sorted order followed by all the consonants of s in sorted order.
+
+Note: vowels are ["a", "e", "i", "o", "u"] and consonants are all other characters.
+
+Constraints
+
+n ≤ 100,000 where n is the length of s
+Example 1
+Input
+s = "decalin"
+Output
+"aeicdln"
+Explanation
+Vowels are "eai" which when sorted is "aei"
+Consonants are "dcln" which when sorted is "cdln"
+Their concatenation is "aeicdln"
+Solved
+1,510
+Attempted
+1,643
+Rate
+91.91%
 """
 def solve(s):
     vow = ""
