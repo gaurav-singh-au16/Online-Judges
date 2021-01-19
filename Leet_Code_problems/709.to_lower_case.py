154879ee5664b1acf2551c26a4ebbddbfac8f9a8
Gaurav Singh <gauravsingh.info@gmail.com>
2021-01-19 23:33:48 +0530

    .\709.to_lower_case.py

---
+++ Leet_Code_problems/709.to_lower_case.py
@@ -0,0 +1,40 @@
+"""
+709. To Lower Case
+Easy
+
+605
+
+1764
+
+Add to List
+
+Share
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+Accepted
+245,445
+Submissions
+306,937
+Seen this question in a real interview before?
+
+Yes
+
+No
+"""
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
